14e46d454d3956e928c04aefe221eb7fda9f1767
Hugo de França Torres Ferreira <f3rreirahugo@hotmail.com>
2025-04-03 11:57:52 -0300

    Cadastrando novo usuario no banco a partir do login com google

--- src/main/java/com/github/Hugoftf/Spring/JPA/security/LoginSocialSuccessHandle.java
+++ src/main/java/com/github/Hugoftf/Spring/JPA/security/LoginSocialSuccessHandle.java
@@ -14,9 +14,12 @@ import org.springframework.security.web.authentication.SavedRequestAwareAuthenti
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 public class LoginSocialSuccessHandle extends SavedRequestAwareAuthenticationSuccessHandler {
+
+    private static final String SENHAPADRAO = "321";
 
     private final UsuarioService usuarioService;
 
@@ -38,12 +41,31 @@ public class LoginSocialSuccessHandle extends SavedRequestAwareAuthenticationSuc
         String email  = oAuth2User.getAttribute("email");
         Usuario usuario = usuarioService.findByEmail(email);
 
-
+        if(usuario == null){
+            usuario = cadastrarUsuario(email);
+        }
 
         authentication = new CustomAuthentication(usuario);
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         super.onAuthenticationSuccess(request, response, authentication);
+    }
+
+    public Usuario cadastrarUsuario(String email){
+
+        Usuario usuario = new Usuario();
+        usuario.setLogin(encurtarLogin(email));
+        usuario.setEmail(email);
+        usuario.setSenha(SENHAPADRAO);
+        usuario.setRoles(List.of("OPERARIO"));
+
+        usuarioService.salvar(usuario);
+
+        return usuario;
+    }
+
+    public String encurtarLogin(String email){
+        return email.substring(0, email.indexOf("@"));
     }
 }
